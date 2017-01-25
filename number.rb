b90059977e6986215ee62e2d8a61d87fd3d28ebc
Tula Ram Subba <tularamsubba@Tulas-MacBook-Air.local>
2017-01-25 17:35:23 -0500

    winningnumber

---
+++ number.rb
@@ -0,0 +1,15 @@
+require "minitest/autorun"
+require_relative "testnumber1.rb"
+
+class TestNumber < Minitest::Test
+
+def test_if_array_is_empty_returns_false
+	assert_equal(false,check_number([1,2,3,4,5],7))
+ end
+ def test_if_array_is_full_returns_true
+	assert_equal(true,check_number([1,2,3,4,5],1))
+ end
+ def test_if_array_is_full_returns_true
+	assert_equal(true,check_number([1,2,3,4,5],2))
+ end
+end
---
+++ testnumber1.rb
@@ -0,0 +1,7 @@
+def check_number(winning_numbers,user_number)
+	if winning_numbers.include?(user_number)
+		true
+	else
+		false
+	end
+end
