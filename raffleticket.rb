ed60f652d33cbf24140fe2a1f974a839f8ebcbbe
Tula Ram Subba <tularamsubba@Tulas-MacBook-Air.local>
2017-01-27 13:20:45 -0500

    raffleticket1

--- raffleticket.rb
+++ raffleticket.rb
@@ -7,10 +7,15 @@ def test_ticket_5423_returns_true
 	my_ticket = "5423"
 	winning_tickets = ["5423"]
 	assert_equal(true, winning_number_include?(my_ticket,winning_tickets))
- end
+	end
 def test_ticket_1393_returns_true
 	my_ticket = "1393"
 	winning_tickets = ["5231", "3836", "1393"]
 	assert_equal(true, winning_number_include?(my_ticket,winning_tickets))
- end
+ 	end
+def test_ticket_5142_returns_false
+	my_ticket = "5142"
+	winning_tickets = ["5231", "3836", "1393", "7263", "5143", "7153"]
+	assert_equal(false, winning_number_include?(my_ticket,winning_tickets))
+ 	end
 end	
