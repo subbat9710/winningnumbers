dede5fca788d122deeb3f2be8125650d140615e1
Tula Ram Subba <tularamsubba@Tulas-MacBook-Air.local>
2017-02-01 10:07:13 -0500

    hwlast

--- testparticipant.rb
+++ testparticipant.rb
@@ -1,17 +1,19 @@
 def participant_number_include?(participant_number,winning_numbers)
-	winning_numbers.include?(participant_number)
-end
-def participant_number_include?(participant_number,winning_numbers)
+	#def participant_number_include?(participant_number,winning_numbers)
+	#winning_numbers.include?(participant_number)
+#end
+#def participant_number_include?(participant_number,winning_numbers)
 	winning_numbers.include?(participant_number)
 	winner = false
 
 	winning_numbers.each do |participant|
 		if participant_number == participant
-			return true
+			return winner = true
 		end
 	end
 	winner
 end
+
 #def pick(n=1)
 	#numbers.sample(n).sort
 #end
@@ -19,4 +21,13 @@ end
 	#puts "The_winning_numbers"
 	#puts pick(1).join "\t"
 	#puts "And_the_winner_is: #{pick}"
+#end
+	#if participant_number.respond_to?("each")
+		#output = []
+		#numbers.each do |winning_numbers|
+			#output << test(winning_numbers)
+	    #end
+	#else
+		#output
+	#end	
 #end
